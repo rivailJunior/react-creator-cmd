1ae8d14ec42b8842f67c3becf97ed69918b6f7f8
rivailJunior <rivail.rj@gmail.com>
2024-01-12 12:05:48 -0400

    fix: lint ts problems

--- dist/template/next-typescript-template/src/components/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Heading = void 0;
-var heading_1 = require("./heading");
-Object.defineProperty(exports, "Heading", { enumerable: true, get: function () { return heading_1.Heading; } });
-//# sourceMappingURL=index.js.map

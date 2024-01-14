f89a1d38d22016952a5ca4779342ed244129a676
rivailJunior <rivail.rj@gmail.com>
2024-01-14 13:28:48 -0400

    fix: template ts

--- src/domain/create-dir-from-template.ts
+++ src/domain/create-dir-from-template.ts
@@ -10,7 +10,7 @@ import { ROOT_DIR_LOCATION } from "../constants";
  */
 export async function copyFolder(folderPath: string) {
   await fs.cpSync(
-    path.resolve(ROOT_DIR_LOCATION + "/dist/template/next-ts-vite-workflow"),
+    path.resolve(ROOT_DIR_LOCATION + "/src/template/next-ts-vite-workflow"),
     folderPath,
     {
       recursive: true,

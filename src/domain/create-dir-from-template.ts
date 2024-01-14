28c5af10d0b283c6f6ffbba55492c4435fb97922
rivailJunior <rivail.rj@gmail.com>
2024-01-14 12:58:20 -0400

    fix: path from cp next project

--- src/domain/create-dir-from-template.ts
+++ src/domain/create-dir-from-template.ts
@@ -10,7 +10,7 @@ import { ROOT_DIR_LOCATION } from "../constants";
  */
 export async function copyFolder(folderPath: string) {
   await fs.cpSync(
-    path.resolve(ROOT_DIR_LOCATION + "/src/template/next-typescript-template"),
+    path.resolve(ROOT_DIR_LOCATION + "/dist/template/next-typescript-template"),
     folderPath,
     {
       recursive: true,

5afe79c8eee9870aba2dd35822dfb4f9944118f7
Rivail Pinto <rivail.rj@gmail.com>
2024-06-25 20:18:07 -0400

    Dev (#42)
    * chore(release): v1.0.0
    
    * fix: env config
    
    * 1.0.1
    
    * fix: validate nextjs project before generate module or route
    
    ---------
    
    Co-authored-by: rivapinto <rivail.santos@trafilea.com>

--- src/domain/usecases/create-dir-from-template.ts
+++ src/domain/usecases/create-dir-from-template.ts
@@ -2,6 +2,37 @@ import fs from "fs";
 import path from "path";
 import { ROOT_DIR_LOCATION } from "../../constants";
 
+const rootDir = process.cwd();
+const checkExists = (filePath) => fs.existsSync(filePath);
+const checkPackageJson = (filePath) => {
+  if (checkExists(filePath)) {
+    const packageJson = require(filePath);
+    return (
+      packageJson.dependencies &&
+      packageJson.dependencies.next &&
+      packageJson.dependencies.react
+    );
+  }
+  return false;
+};
+
+const checkIfIsNextJsProject = () => {
+  // Define the paths to check
+  const packageJsonPath = path.join(rootDir, 'package.json');
+  const nextConfigJs = path.join(rootDir, 'next.config.js');
+  const nextConfigMjs = path.join(rootDir, 'next.config.mjs');
+
+  // Perform the checks
+  const isNextJsProject =
+    checkPackageJson(packageJsonPath) &&
+    (checkExists(nextConfigJs) || checkExists(nextConfigMjs));
+
+  // Output the result
+  if (!isNextJsProject) {
+    throw 'You must be in a Nextjs project.';
+  }
+};
+
 /**
  * Copies a folder from the source path to the destination path.
  *
@@ -16,10 +47,11 @@ export function copyProject(folderPath: string, pathRoute: string) {
 }
 
 export function copyRouter(routerName: string) {
+  checkIfIsNextJsProject();
   if (fs.existsSync(routerName))
     throw new Error(`${routerName} already exists`);
   return fs.cpSync(
-    path.resolve(ROOT_DIR_LOCATION + "/src/template/route-template"),
+    path.resolve(ROOT_DIR_LOCATION + '/src/template/route-template'),
     routerName,
     {
       recursive: true,
@@ -28,12 +60,13 @@ export function copyRouter(routerName: string) {
 }
 
 export function copyHttpModule() {
-  const folderPath = "modules/http-module";
+  checkIfIsNextJsProject();
+  const folderPath = 'modules/http-module';
   if (fs.existsSync(folderPath))
     throw new Error(`${folderPath} already exists`);
   return fs.cpSync(
     path.resolve(
-      ROOT_DIR_LOCATION + "/src/template/modules-template/http-module"
+      ROOT_DIR_LOCATION + '/src/template/modules-template/http-module'
     ),
     folderPath,
     {
